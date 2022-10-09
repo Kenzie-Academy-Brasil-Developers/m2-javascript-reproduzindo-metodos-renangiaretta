19bcb1bfd6d952bcc31f21eb4332db473a128aab
renangiaretta <ranan.giaretta@hotmail.com>
2022-10-09 12:21:34 -0300

    correcao

--- src/scripts/metodos.js
+++ src/scripts/metodos.js
@@ -1,61 +1,117 @@
-// const arrTeste = [1,2,3]
+
+const callbackMap = (n => n * 2)
+const arrMap = [1,2,3]
 
 function recreateMap(array, callbackFunc){
     let arrResult = [];
     for(let i = 0; i < array.length; i++){
-        showResult.push(callbackFunc(array[i]))
+        arrResult.push(callbackFunc(array[i]))
     }
     return arrResult;
 }
 
+console.log('TESTANDO FUNÇÃO MAP')
+console.log(recreateMap(arrMap, callbackMap))
 
-function recreateFilter(array, callbackFunc){
-    let arrResult = [];
-    for(let i = 0; i < array.length; i++){
-        if(callbackFunc(array[i])){
-            arrResult.push(array[i], i)
-        }
-    }
-    return arrResult;
+
+
+const ages = [32, 33, 16, 40];
+
+function callbackFilter(age) {
+  return age >= 18;
 }
 
-
-function recreateReduce(array){
-    let result = 0;
-    for(let i = 0; i < array.length; i++){
-        result += array[i]
-        }
-        return result
-    }
-
-
-
-function recreateFind (array, callbackFunc){
-    let result = 0;
-    for(let i = 0; i < array.length; i++){
-        if(callbackFunc(array[i])){
-            result = array[i];
-            break;
-        }
-    }
-    return result;
-}
-
-
-function recreateIncludes(array, callbackFunc){
+function recreateFilter(array, callback) {
+    let newArr = []
     for(let i = 0; i<array.length; i++){
-        if((array[i]) == callbackFunc){
-            return true;
+        if(callback(array[i])){
+            newArr.push(array[i])
+        }
+    }
+    return newArr
+}
+console.log('TESTANDO FUNÇÃO FILTER')
+console.log(recreateFilter(ages, callbackFilter))
+
+
+
+function callbackFind(age) {
+    return age >= 33;
+  }
+  
+  function recreateFind(array, callback) {
+      let result = 0;
+      for(let i=0; i<array.length; i++){
+          if(callback(array[i])){
+              result = array[i];
+              break;
+          }
+      }
+      return result;
+  }
+  console.log('TESTANDO FUNÇÃO FIND')
+  console.log(recreateFind(ages, callbackFind))
+
+
+
+const callbackIndexOf = (value) => {
+    for (let i = 0; i<= ages.length; i++){
+        if (value == ages[i]){
+            return value;
         }
     }
 }
 
-
-function recreateIndexOf(array, callbackFunc) {
-    for(let i = 0; i < array.length; i++){
-        if((array[i]) == callbackFunc){
+function recreateIndexOf(array, callback) {
+    for(let i=0; i<array.length; i++){
+        if((array[i]) == callback){
             return i;
         }
     }
+    return -1
+}
+console.log('TESTANDO FUNÇÃO INDEXOF')
+console.log(recreateIndexOf(ages, callbackIndexOf(32)))
+console.log(recreateIndexOf(ages, callbackIndexOf(16)))
+console.log(recreateIndexOf(ages, callbackIndexOf(3)))
+
+
+
+
+const callbackIncludes = (value) => {
+    for (let i = 0; i<=ages.length; i++){
+       if (value == ages[i]){
+            return value;
+        }
+    }
 }
 
+function recreateIncludes(array, callback) {
+    for(let i=0; i<array.length; i++){
+        if(array[i]==callback){
+            return true;
+        }
+    }
+return false
+}
+console.log('TESTANDO FUNÇÃO INCLUDES')
+console.log(recreateIncludes(ages, callbackIncludes(32)))
+console.log(recreateIncludes(ages, callbackIncludes(40)))
+console.log(recreateIncludes(ages, callbackIncludes(50)))
+
+
+
+function callbackReduce(acumulator, actualValue) {
+    return acumulator + actualValue;
+  }
+  
+  function recreateReduce(array, callback, initialValue = 0) {
+    let sum = initialValue
+    for(let i = 0; i < array.length; i++){
+      sum = callback(sum, array[i])
+    }
+    return sum
+  }
+console.log('TESTANDO FUNÇÃO REDUCE')
+console.log(recreateReduce(ages, callbackReduce));
+console.log(recreateReduce(ages, callbackReduce, 100));
